9e177cc102dad262bf102ff79fa9a95d99989e5d
JujubaOhime <ameliadag96@gmail.com>
2024-04-28 16:57:31 -0300

    refactor: simplify signup code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,13 +2,13 @@ import { type HttpResponse, type HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
+    const requiredFields = ['name', 'email']
+    requiredFields.forEach(field => {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    })
   }
 }
